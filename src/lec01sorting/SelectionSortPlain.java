f045f4905ba383ecbaec535ae56bbd140f88f66b
webturing <zj@webturing.com>
2020-03-09 09:54:34 +0800

    选择排序非递归版本：每次从无序区选择一个最小值和无序区第一个元素置换

---
+++ src/lec01sorting/SelectionSortPlain.java
@@ -0,0 +1,28 @@
+package lec01sorting;
+
+import java.util.Arrays;
+
+
+public class SelectionSortPlain {
+    static void sort(int arr[], int left, int right) {
+        for (int i = left; i <= right; i++) {
+            int k = i;
+            for (int j = i + 1; j <= right; j++) {
+                if (arr[j] < arr[k]) k = j;
+
+            }
+            if (k != i) {
+                int temp = arr[i];
+                arr[i] = arr[k];
+                arr[k] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 3, 5, 7, 9, 2, 4, 6, 8, 0};
+        System.out.println(Arrays.toString(arr));
+        sort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
+    }
+}
