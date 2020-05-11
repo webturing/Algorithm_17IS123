75337d0be831c23fde24b42b9fc795ae98807624
webturing <zj@webturing.com>
2020-05-11 19:14:40 +0800

    第8讲java数据结构应用

--- src/lec08ds2/HeapDemo.java
+++ src/lec08ds2/HeapDemo.java
@@ -5,11 +5,14 @@ import java.util.PriorityQueue;
 public class HeapDemo {
     public static void main(String[] args) {
         Integer[] arr=new Integer[]{1,3,5,7,9,2,4,6,8,0};
-        PriorityQueue<Integer> p=new PriorityQueue<>();
-        for(int i:arr)p.add(i);
-        while(!p.isEmpty()){
-            System.out.print(p.peek());
-            p.poll();
+        PriorityQueue<Integer>  Q=new PriorityQueue<>();//一个空的小顶堆
+        for(Integer i:arr){
+            Q.offer(i);
+        }
+        while(!Q.isEmpty()){
+            System.out.println(Q.peek());//堆顶元素，最小值
+
+            Q.poll();// 堆顶元素删除
         }
 
     }
---
+++ src/lec08ds2/XiaoMing2.java
@@ -0,0 +1,26 @@
+package lec08ds2;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class XiaoMing2 {
+    public static void main(String[] args) {
+        int[] a = new int[]{1, 2, 3, 4, 5, 6, 7};
+        //(1,2,3,4,5,6,7)=> (0,3,3,4,5,6,7)
+        int n=a.length;
+        int tot=0;
+        PriorityQueue<Integer> Q=new PriorityQueue<>();
+        for(int i=0;i<n;i++){
+            Q.offer(a[i]);
+        }
+        while(Q.size()>1){
+            int x=Q.peek();
+            Q.poll();
+            int y=Q.peek();
+            Q.poll();
+            tot+=x+y;
+            Q.offer(x+y);
+        }
+        System.out.println(tot);
+    }
+}
