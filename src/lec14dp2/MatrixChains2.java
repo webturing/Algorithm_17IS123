c79963e67ddf19b31137a2088f2563bf207ca964
webturing <zj@webturing.com>
2020-05-28 07:46:45 +0800

    最优矩阵连乘dp2

--- src/lec14dp2/MatrixChains2.java
+++ src/lec14dp2/MatrixChains2.java
@@ -3,26 +3,28 @@ package lec14dp2;
 public class MatrixChains2 {
     public static void main(String[] args) {
         int n = 6;
-        int[] M = new int[]
+        int[] p = new int[]
                 {30, 35, 15, 5, 10, 20, 25};
 
-        int[][] dp = new int[n + 1][n + 1];
+        int[][] m = new int[n + 1][n + 1];
         for (int i = 1; i <= n; i++)
             for (int j = 1; j <= n; j++) {
                 if (i == j)
-                    dp[i][j] = 0;
+                    m[i][j] = 0;
                 else
-                    dp[i][j] = Integer.MAX_VALUE;
+                    m[i][j] = Integer.MAX_VALUE;
             }
-
-        for (int i = 2; i <= n; i++) {
-            for (int j = i + 1; j <= n; j++) {
-                for (int k = i; k <= j - 1; k++) {
-                    dp[i][j] = Math.min(dp[i][j], dp[i][k] + dp[k + 1][j] + M[i - 1] * M[k] * M[j]);
+        for (int r = 2; r <= n; r++) {
+            for (int i = 1; i <= n - r + 1; i++) {
+                int j = i + r - 1;
+                m[i][j] = m[i + 1][j] + p[i - 1] * p[i] * p[j];
+                for (int k = i + 1; k < j; k++) {
+                    m[i][j] = Math.min(m[i][j], m[i][k] + m[k + 1][j] + p[i - 1] * p[k] * p[j]);
                 }
             }
         }
-        System.out.println(dp[1][n]);
+
+        System.out.println(m[1][n]);
     }
 }
 
